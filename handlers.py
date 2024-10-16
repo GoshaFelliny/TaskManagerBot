1f2414021deeb512f0f91537bad65a99305ebf3c
GoshaFelliny <semen.ss62@gmail.com>
2024-10-16 21:29:23 +0300

    Кнопка и малые текстовые команды

--- handlers.py
+++ handlers.py
@@ -1,8 +1,13 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+import config
+from aiogram import Bot
 
 router = Router()
+bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
 @router.message(Command('start'))
@@ -10,18 +15,20 @@ async def start_handler(msg: Message):
     await msg.answer("Привет я бот который поможет с заметками!")
 
 
-from aiogram.types import InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder, KeyboardBuilder, InlineKeyboardMarkup
+import kb
 
 
 @router.message(Command('task'))
 async def new_task_handler(msg: Message):
+    await msg.answer("Введи текст задачи, а затем выбери время", reply_markup=kb.cancel_keyboard)
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Отмена", callback_data="cancel_task")]
-    ])
 
-    await msg.answer("Введи текст задачи, а затем выбери время", reply_markup=keyboard)
+@router.callback_query(lambda c: c.data == 'cancel_task')
+async def cancel_task(callback_query: CallbackQuery):
+    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+
+    await callback_query.answer("Задача отменена")
+
 
 @router.message()
 async def message_handler(msg: Message):
--- kb.py
+++ kb.py
@@ -1,1 +1,7 @@
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, KeyboardBuilder, InlineKeyboardMarkup
+
+cancel_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Отмена", callback_data="cancel_task")]
+])
 
